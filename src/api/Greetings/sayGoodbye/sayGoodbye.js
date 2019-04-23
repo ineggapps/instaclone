258062e66638b42aa732d6f9e2a9a815fd632b80
inegg <inegg.apps@gmail.com>
2019-04-23 21:36:55 +0900

    Setting up project with Prisma
    1. Prisma Module was installed in my project.
    
    2. GraphQL Schema was splited by logical unit which exists on some folders for classification.

---
+++ src/api/Greetings/sayGoodbye/sayGoodbye.js
@@ -0,0 +1,5 @@
+export default {
+  Query: {
+    sayGoodbye: () => "T^F Good BYE...."
+  }
+};
--- src/server.js
+++ src/server.js
@@ -1,25 +1,11 @@
 require("dotenv").config();
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
+import schema from "./schema";
 
 const PORT = process.env.PORT || 1000;
 
-const typeDefs = `
-    type Query{
-        hello: String!
-    }
-`;
-
-const resolvers = {
-  Query: {
-    hello: () => "Hi"
-  }
-};
-
-const server = new GraphQLServer({
-  typeDefs,
-  resolvers
-});
+const server = new GraphQLServer({ schema });
 
 server.express.use(logger("dev"));
 server.start({ port: PORT }, () => console.log(`Server running on http://localhost:${PORT}`));
