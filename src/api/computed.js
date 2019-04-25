49d1886389ed69cd056c35f5804e1921237e5d70
inegg <inegg.apps@gmail.com>
2019-04-25 22:20:33 +0900

    Bug fix for following and implemented isLiked
    Bug fix for following and implemented isLiked

---
+++ src/api/computed.js
@@ -0,0 +1,48 @@
+import { prisma } from "../../generated/prisma-client";
+
+export default {
+  User: {
+    fullName: parent => {
+      return `${parent.firstName} ${parent.lastName}`;
+    },
+    isFollowing: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      try {
+        return prisma.$exists.user({
+          AND: [{ id: user.id }, { following_some: { id: parentId } }]
+        });
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+    isSelf: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      return user.id === parentId;
+    }
+  },
+  Post: {
+    isLiked: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id } = parent;
+      const isLiked = await prisma.$exists.like({
+        AND: [
+          {
+            user: {
+              id: user.id
+            }
+          },
+          {
+            post: {
+              id
+            }
+          }
+        ]
+      });
+      console.log(isLiked);
+      return isLiked;
+    }
+  }
+};
